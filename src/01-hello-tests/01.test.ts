2ffb75ecb4d8aca5182ba5d12c8132911fc5d7e7
feddorovich <feddorovich@outlook.com>
2023-02-02 18:02:39 +0300

    first commit

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,18 @@
+import {sum} from "./01";
+
+test('sum should be correct', () => {
+    // data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    // action
+    const result1 = sum(a, b);
+    const result2 = sum(b, c)
+
+    // expect result
+    expect(result1).toBe(3);
+    expect(result2).toBe(5)
+
+})
+
---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,16 @@
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+}
+
+
+
+
+const sentense = "Hello my friend!";
+
+export function splitIntoWords(sentense: string) {
+    return ['hello', 'my', 'friend']
+}
