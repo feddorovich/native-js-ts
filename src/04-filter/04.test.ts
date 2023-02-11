2b4cf13d3ff86aafaba0afa8cdbed076fb092460
feddorovich <feddorovich@outlook.com>
2023-02-11 15:44:32 +0300

    04 filter

--- src/04-filter/04.test.ts
+++ src/04-filter/04.test.ts
@@ -4,11 +4,7 @@ import {CourseType} from "./04";
 test('should take old men older then 90', () => {
     const ages = [18, 20, 22, 100, 1, 90, 14]
 
-    const predicate = (age: number) => {
-        return age > 90;
-    }
-
-    const oldAges = ages.filter(predicate)
+    const oldAges = ages.filter(age => age > 90)
 
     expect(oldAges.length).toBe(1)
     expect(oldAges[0]).toBe(100)
@@ -21,14 +17,38 @@ test('should take chiper 160', () => {
         {title: 'REACT', price: 150}
     ]
 
-
-    const chipPredicate = (course: CourseType) => {
-        return course.price < 160
-    }
-
-    const chipCourses = courses.filter(chipPredicate)
+    const chipCourses = courses.filter(course => course.price < 160)
 
     expect(chipCourses.length).toBe(2)
     expect(chipCourses[0].title).toBe("CSS")
     expect(chipCourses[1].title).toBe("REACT")
+})
+
+test ('get only completed tasks', ()=> {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Bolt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const completedTasks = tasks.filter(task => task.isDone)
+
+    expect(completedTasks.length).toBe(2)
+    expect(completedTasks[0].id).toBe(2)
+    expect(completedTasks[1].id).toBe(4)
+})
+test ('get only uncompleted tasks', ()=> {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Bolt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const unCompletedTasks = tasks.filter(task => !task.isDone)
+
+    expect(unCompletedTasks.length).toBe(2)
+    expect(unCompletedTasks[0].id).toBe(1)
+    expect(unCompletedTasks[1].id).toBe(3)
 })
